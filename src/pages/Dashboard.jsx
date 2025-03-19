96683421dffbda23e134522642e00889cdf82e44
allam <allamirshad8@gmail.com>
2025-03-19 20:12:47 +0530

    chANGE

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,13 @@
 
 
 
-export default function DashboardsPage{
+export default function DashboardsPage(){
     const [iframeLoaded, setIframeLoaded] = useState(false);
     return (
         <div className= "w-full h-[80vh]">
           {iframeLoaded && (
             <iframe
-            //   src="https://lookerstudio.google.com/embed/reporting/5841aa92-4199-44fc-884f-2021b478a8f1/page/y2VAF"
+              src="https://lookerstudio.google.com/embed/reporting/5841aa92-4199-44fc-884f-2021b478a8f1/page/y2VAF"
               width="100%"
               height="600"
               frameBorder="0"
@@ -16,4 +16,6 @@ export default function DashboardsPage{
               sandbox="allow-scripts allow-same-origin"
             />
           )}
+        </div>
+      )
 }
